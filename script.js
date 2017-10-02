b59ecefe52778d4358626ecd9d97e20b58a33aa2
Michael van den Oudenalder <michael.oudenalder@valtech.nl>
2017-10-02 19:32:00 +0200

    Spent close to 2 hours cleaning up the code, adding comments and actually looking up the right album pictures.

--- script.js
+++ script.js
@@ -1,75 +1,90 @@
 ;(function() {
 	'use strict';
 
-	// Semi-global variables
-	var albumData = {},
-			photosData = {},
-			albumList = document.querySelector('.albums'),
-			albumBaseURL = 'https://jsonplaceholder.typicode.com/albums',
-			photosBaseURL = 'https://jsonplaceholder.typicode.com/photos?';
-
+	/**
+	* Fetch albums and photos, using Axios
+	* @return {undefined} undefined
+	*/
 	function fetchData() {
+		var albums = [],
+				baseURL = 'https://jsonplaceholder.typicode.com';
+
 		// fetch albums
-		axios.get(albumBaseURL)
+		axios.get(baseURL + '/albums')
 			.then(function(response) {
 
 				// Get the first 5 albums
-				albumData = extractSubArray(response.data, 5); // [{}, {}, {}, {}, {}]
+				albums = response.data.slice(0, 5); // [{}, {}, {}, {}, {}]
 
-				// Extract album IDs to construct photos GET call
-				var albumIDs = albumData.map(extractID); // [1, 2, 3, 4, 5]
+				// Build the url for the GET call for the photos, using the album IDs
+				var photosUrl = buildPhotosUrl(albums, baseURL);
 
-				// Build photos GET call
-				var photosUrlParamString = albumIDs.reduce(buildUrl, photosBaseURL);
-
-				// Get the 5 photos matching the albums
-				axios.get(photosUrlParamString)
+				// Fetch photos
+				axios.get(photosUrl)
 					.then(function(response) {
-						photosData = response.data;
-						appendDataToDOM();
+						var photos = response.data;
+						appendPhotosToAlbums(photos, albums);
+						appendAlbumsToDOM(albums);
 					})
 					.catch(function(error) {
 						console.log(error);
 					});
-
 			})
 			.catch(function(error) {
 				console.log(error);
 			});
-
-		// Helper functions
-		function extractSubArray(data, amount) {
-			return data.slice(0, amount);
-		}
-
-		function extractID(obj) {
-			return obj.id;
-		}
-
-		function buildUrl(accumulated, current, index, array) {
-			var paramString = accumulated + 'id=' + current,
-					ampersand = index >= (array.length - 1) ? '' : '&';
-			paramString += ampersand;
-			return paramString;
-		}
 	}
 
-	function appendDataToDOM() {
-		albumData.forEach(addAlbumToDOM);
+	/**
+	* Build url and parameters for the photos ajax call
+	* @param {Array} albums array of albums
+	* @return {String} url including parameters as String
+	*/
+	function buildPhotosUrl(albums, baseURL) {
+		// Extract album IDs
+		var albumIDs = albums.map(function(obj) {
+			return obj.id;
+		});
 
-		function addAlbumToDOM(obj, index) {
+		// Add the album IDs as parameters to the url and return it
+		return baseURL + '/photos?albumId=' + albumIDs.join('&albumId=');
+	}
+
+	/**
+	* Add photo url to albums
+	* @param {Array} albums array of albums
+	* @param {Array} photos array of photos
+	* @return {undefined} undefined
+	*/
+	function appendPhotosToAlbums(photos, albums) {
+		albums.forEach(function(album) {
+			var matchingPhoto = photos.find(function(photo) {
+				return photo.albumId === album.id;
+			});
+			album.url = matchingPhoto.url;
+		});
+	}
+
+	/**
+	* Append album & photo to DOM
+	* @param {Array} albums array of albums
+	* @return {undefined} undefined
+	*/
+	function appendAlbumsToDOM(albums) {
+		var albumList = document.querySelector('.albums');
+		albums.forEach(function(obj) {
 			var li = document.createElement('li'),
 					image = document.createElement('img'),
 					title = document.createElement('h2');
 
-			image.src = photosData[index].url;
+			image.src = obj.url;
 			title.innerHTML = obj.title;
 
 			li.appendChild(image);
 			li.appendChild(title);
 
 			albumList.appendChild(li);
-		}
+		});
 	}
 
 	function init() {
